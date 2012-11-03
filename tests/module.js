b892a2590a3492156adbb57abbc3e91e7e52c64d
sirprize <hoegl@backdoor.ch>
2012-11-03 10:06:48 +0100

    Add unit tests

---
+++ tests/module.js
@@ -0,0 +1,16 @@
+define([
+    "require",
+    "doh/main"
+], function (require, doh) {
+    "use strict";
+    
+    if(doh.isBrowser) {
+        doh.register("dijitive/Button", "../../../../tests/Button.html", 30000);
+        doh.register("dijitive/Checkbox", "../../../../tests/Checkbox.html", 30000);
+        doh.register("dijitive/ExpandingTextarea", "../../../../tests/ExpandingTextarea.html", 30000);
+        doh.register("dijitive/Radio", "../../../../tests/Radio.html", 30000);
+        doh.register("dijitive/Select", "../../../../tests/Select.html", 30000);
+        doh.register("dijitive/Textarea", "../../../../tests/Textarea.html", 30000);
+        doh.register("dijitive/Textbox", "../../../../tests/Textbox.html", 30000);
+	}
+});
